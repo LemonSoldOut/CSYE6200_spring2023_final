4cadfd6e1828349a344fe83c492f4303a16480c6
Cheng <lemonsoldout@163.com>
2023-03-05 21:19:58 -0500

    :wrench: 添加有道单词发音在线 API

--- src/edu/northeastern/csye6200/Test.java
+++ src/edu/northeastern/csye6200/Test.java
@@ -10,5 +10,9 @@ package edu.northeastern.csye6200;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello CSYE 6200!");
+		String word = "hello";
+		String audioAPI = "http://dict.youdao.com/dictvoice?audio=" + word;
+		String dictCSVPath = "https://raw.githubusercontent.com/skywind3000/ECDICT/master/ecdict.csv";
+		
 	}
 }
